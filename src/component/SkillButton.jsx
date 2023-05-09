4bab6dd0b3d911483996ab1cee293b6049539dd7
juhasznellee <juhasz.nelli16@gmail.com>
2023-05-09 02:11:50 +0200

    Skill gomb helyének módosítása

--- src/component/SkillButton.jsx
+++ src/component/SkillButton.jsx
@@ -5,10 +5,10 @@ export default function SkillButton (props){
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    if(props.button === 1 && props.active === true){    
-        return <div onClick={e => props.onclick(1)} className='skill-circle' style={{marginTop: randomNumber(1, 36) + '%' , marginLeft: randomNumber(1, 22) + '%'}}></div>
+    if(props.button === 1 && props.active === true){   
+        return <div onClick={e => props.onclick(1)} className='skill-circle' style={{marginTop: randomNumber(1, 36) + '%' , marginLeft: randomNumber(1, 21) + '%'}}></div>
     }if(props.button === 2 && props.active === true){ 
-        return <div onClick={e => props.onclick(2)}className='skill-circle' style={{marginTop: randomNumber(12, 36) + '%' , marginLeft: randomNumber(75, 97) + '%'}}></div>
+        return <div onClick={e => props.onclick(2)}className='skill-circle' style={{marginTop: randomNumber(12, 36) + '%' , marginLeft: randomNumber(76, 97) + '%'}}></div>
     }else{
         return <></>
     }
