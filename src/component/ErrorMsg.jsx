8d3f8aa7c4db0be9ef1d7bb3ac173f4650ec0f1c
juhasznellee <juhasz.nelli16@gmail.com>
2023-05-10 20:32:11 +0200

    Hibaüzi javítása

--- src/component/ErrorMsg.jsx
+++ src/component/ErrorMsg.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function ErrorMsg (props){
     if(props.place === 'investment' && props.active === true){
         return "Erre nincs elegendő pénzem!";
